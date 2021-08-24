a9b19f2a7d16f1e413deb9de09f734687b00b2af
ORCT <sonic407@naver.com>
2021-08-24 16:43:39 +0900

    edit 4673

--- boj_4673[S5].py
+++ boj_4673[S5].py
@@ -1,16 +1,24 @@
 #The main logic is int(n)+n[i]... = m and m is not the self number
 #You have to find self number
-def self_num(n:int):
-    return
+def sum_digits(n:int):
+    sum_of_digits=0
+    for i in range(len(str(n))):
+        digit=str(n)[i]
+        sum_of_digits += int(digit)
+    return sum_of_digits
 
 def make_num_sequence(n:int):
-    num_sequence = []
-    for i in range(1,10001):
-        int(n)+int(n[i])
-    return
+    d = n+sum_digits(n)
+    return d
 
-def make_whole_num(n:int):
-    whole_num = []
-    for i in range(1,n+1):
-        whole_num.append[i]
-    return whole_num
+num_list = []
+for i in range(1, 10001):
+    num_list.append(i)
+for i in range(1, 10001):
+    remove_num = make_num_sequence(i)
+    try:
+        num_list.remove(remove_num)
+    except ValueError:
+        pass
+for i in range(len(num_list)):
+    print(num_list[i])
