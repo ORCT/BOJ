b47e1fa314697c6af4fa3509899c7237f94fd825
ORCT <sonic407@naver.com>
2021-10-22 12:19:28 +0900

    new

--- boj_2231_B2.py
+++ boj_2231_B2.py
@@ -1,4 +1,14 @@
-def find_digit_generator(n):
-    len_num = len(str(n))
-    a = n%10 # digit 1
-    return
+def find_generator(n):
+    count = 0
+    for i in range(int(n)):
+        check = i
+        for j in str(i):
+            check += int(j)
+        if check == int(n):
+            print(i)
+            count += 1
+            break
+    if count == 0:
+        print('0')
+N = input()
+find_generator(N)
