8f08d8f3f4760e193fa45e8e332ccaf1ef1f279e
ORCT <sonic407@naver.com>
2021-12-13 11:52:59 +0900

    back tracking

--- boj_15651.py
+++ boj_15651.py
@@ -1,22 +1,19 @@
 import sys
 ssr = sys.stdin.readline
 
-n,m = map(int, ssr().split())
+n,m = map(int,ssr().split())
 check_list = [False for _ in range(n+1)]
 arr = []
 arr_len = 0
-num=1
 
-def func(num, arr_len):
+def func(arr_len):
     if arr_len == m:
         print(*arr)
         return
-    for i in range(num,n+1):
+    for i in range(1,n+1):
         if not check_list[i]:
             arr.append(i)
-            check_list[i] = True
-            func(i+1,arr_len + 1)
+            func(arr_len+1)
             arr.pop()
-            check_list[i] = False
             
-func(num, arr_len)
+func(arr_len)
