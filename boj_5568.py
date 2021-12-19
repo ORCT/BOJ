61e9e8d39d4830d9c8b4311cf338324227c323c6
ORCT <sonic407@naver.com>
2021-12-19 09:54:51 +0900

    test clone

---
+++ boj_5568.py
@@ -0,0 +1,13 @@
+import sys
+ssr = sys.stdin.readline
+#중복된 정수는 한개로 쳐야하나봄
+
+def make_int(num : list):
+    
+    return
+
+n = int(ssr())
+k = int(ssr())
+num = [int(ssr()) for i in range(n)]
+#print(*num)
+make_int(num)
