739cf4800ba9736eac35770aacad779f97410930
ORCT <sonic407@naver.com>
2021-12-23 15:15:56 +0900

    O(1)

---
+++ boj_2164.py
@@ -0,0 +1,13 @@
+import sys
+ssr = sys.stdin.readline
+
+input = int(ssr()) 
+square = 2 
+while True: 
+    if (input == 1 or input == 2): 
+        print(input) 
+        break 
+    square *= 2 
+    if (square >= input): 
+        print((input - (square // 2)) * 2) 
+        break
