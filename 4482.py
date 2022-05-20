8af12030c445dc9b122039a3a331817ba145bf6e
ORCT <sonic407@naver.com>
2022-05-20 17:48:21 +0900

    dp practice

---
+++ 4482.py
@@ -0,0 +1,12 @@
+t = int(input())
+sub = [0 for _ in range(1000)]
+sub1 = [0 for _ in range(1000)]
+for i in range(1,1000):
+    sub[i] = sub[i-1]+i
+    sub1[i] = sub1[i-1]+sub[i]
+for i in range(t):
+    n = int(input())
+    print(f'{i+1}: {n} {sub1[n]}')
+    
+#1,3,6,10,15,21
+#1,4,10,20,35
