aba363c5e82fbc8605b84fa2c0a5ae85fc1d89bd
ORCT <sonic407@naver.com>
2022-06-03 19:18:59 +0900

    dp

---
+++ 16395.py
@@ -0,0 +1,7 @@
+t = [[0 for _ in range(31)] for _ in range(31)]
+t[1][1] = 1
+for i in range(2,31):
+    for j in range(1,31):
+        t[i][j] = t[i-1][j-1] + t[i-1][j]
+n,k = map(int, input().split())
+print(t[n][k])
