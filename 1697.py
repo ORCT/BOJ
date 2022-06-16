81d5a14be48c0f79e1b8ce56cba9bc4d4045529d
ORCT <sonic407@naver.com>
2022-06-16 23:02:57 +0900

    bfs

---
+++ 1697.py
@@ -0,0 +1,40 @@
+# def sol(pos,sec):
+#     if pos == 0:
+#         return
+#     elif pos == k:
+#         return sec
+#     sol(pos-1,sec+1)
+#     sol(pos+1,sec+1)
+#     sol(2*pos,sec+1)
+
+# n,k = map(int,input().split())
+# ans = []
+# ans.append(sol(n,0))
+# print(min(ans))
+from collections import deque
+
+n,k = map(int, input().split())
+q = deque([[n,0]])
+visited = [False for _ in range(100001)]
+visited[n] = True
+while 1:
+    if n == k:
+        print(0)
+        break
+    p = q.popleft()
+    a = p[0]-1
+    b = p[0]+1
+    c = p[0]*2
+    if a == k or b == k or c == k:
+        print(p[1]+1)
+        break
+    else:
+        if a>=0 and visited[a] == False:
+            q.append([a,p[1]+1])
+            visited[a] = True
+        if b<=100000 and visited[b] == False:
+            q.append([b,p[1]+1])
+            visited[b] = True
+        if c<=100000 and visited[c] == False:
+            q.append([c,p[1]+1])
+            visited[c] = True
