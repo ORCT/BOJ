0723fd04de629ba9db2a7d3920765dfbf487b1a5
ORCT <sonic407@naver.com>
2022-07-27 10:25:00 +0900

    divide&conquer

---
+++ 1629.py
@@ -0,0 +1,10 @@
+def sol(a,b,c):
+    if b == 1:
+        return a%c
+    elif b%2 == 0:
+        return (sol(a,b//2,c)**2)%c
+    else:
+        return (sol(a,b//2,c)**2)*a%c
+
+a,b,c = map(int, input().split())
+print(sol(a,b,c))
