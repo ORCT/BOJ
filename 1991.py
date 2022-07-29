b574a3246f984513eff74cd036a22c1ad0e64322
ORCT <sonic407@naver.com>
2022-07-29 16:45:47 +0900

    tree, recursion

--- 1991.py
+++ 1991.py
@@ -1,3 +1,57 @@
+from string import ascii_uppercase
 import sys
 ssr = sys.stdin.readline
 
+def preorder(v):
+    global ans
+    if visited[v] == 1:
+        return
+    visited[v] = 1
+    ans += str(v)
+    for i in edges:
+        if i[0] == v:
+            for j in range(1,3):
+                if i[j] != '.':
+                    preorder(i[j])
+
+def inorder(v):
+    global ans
+    if visited[v] == 2:
+        return
+    visited[v] = 2
+    for i in edges:
+        if i[0] == v:
+            if i[1] != '.':
+                inorder(i[1])
+            ans += i[0]
+            if i[2] != '.':
+                inorder(i[2])
+            
+def postorder(v):
+    global ans
+    if visited[v] == 3:
+        return
+    visited[v] = 3
+    for i in edges:
+        if i[0] == v:
+            for j in range(1,3):
+                if i[j] != '.':
+                    postorder(i[j])
+            ans += i[0]
+                    
+n = int(ssr())
+edges = [ssr().rstrip().split() for _ in range(n)]
+visited = {i:0 for i in ascii_uppercase}
+ans = ''
+
+preorder('A')
+print(ans)
+ans = ''
+
+inorder('A')
+print(ans)
+ans = ''
+
+postorder('A')
+print(ans)
+ans = ''
