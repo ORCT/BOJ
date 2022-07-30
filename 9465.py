54c1e9c258636b8459d5eba0934c515df4154a66
ORCT <sonic407@naver.com>
2022-07-30 19:54:28 +0900

    dp

--- 9465.py
+++ 9465.py
@@ -7,7 +7,8 @@ for _ in range(t):
     sticker = [list(map(int, ssr().split())) for _ in range(2)]
     dp = [[0 for _ in range(n)] for _ in range(2)]
     dp[0][0], dp[1][0] = sticker[0][0], sticker[1][0]
-    dp[0][1], dp[1][1] = dp[1][0]+sticker[0][1], dp[0][0]+sticker[1][1]
+    if n > 1:
+        dp[0][1], dp[1][1] = dp[1][0]+sticker[0][1], dp[0][0]+sticker[1][1]
     for i in range(2,n):
         dp[0][i] = max(dp[1][i-1], dp[1][i-2])+sticker[0][i]
         dp[1][i] = max(dp[0][i-1], dp[0][i-2])+sticker[1][i]
