7dded93df07742341d21b62ae7788cf3417338ce
ORCT <sonic407@naver.com>
2022-10-22 15:12:18 +0900

    math

---
+++ 17256.kt
@@ -0,0 +1,13 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`)
+    var ax = sc.nextInt()
+    var ay = sc.nextInt()
+    var az = sc.nextInt()
+    var cx = sc.nextInt()
+    var cy = sc.nextInt()
+    var cz = sc.nextInt()
+    println("${cx-az} ${cy/ay} ${cz-ax}")
+
+}
