2a4d702fea043c1087fb36749328353b2e79285d
ORCT <sonic407@naver.com>
2022-10-28 14:35:43 +0900

    math

---
+++ 20254.kt
@@ -0,0 +1,10 @@
+import java.util.*
+
+fun main(){
+    val sc = Scanner(System.`in`)
+    var a = sc.nextInt()
+    var b = sc.nextInt()
+    var c = sc.nextInt()
+    var d = sc.nextInt()
+    println("${a*56 + b*24 + c*14 + d*6}")
+}
