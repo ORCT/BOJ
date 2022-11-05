8c8dc46a5d4db75dab9beacf750b2d91ea46f317
ORCT <sonic407@naver.com>
2022-11-05 17:16:10 +0900

    math

---
+++ 21300.kt
@@ -0,0 +1,12 @@
+import java.util.*
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    var b = sc.nextInt()
+    var m = sc.nextInt()
+    var p = sc.nextInt()
+    var c = sc.nextInt()
+    var s = sc.nextInt()
+    var w = sc.nextInt()
+    println("${b*5 + m*5 + p*5 + c*5 + s*5 + w*5}")
+}
