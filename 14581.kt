645b328bab87fefd36f8223563df95f3a83005fe
ORCT <sonic407@naver.com>
2022-11-22 02:45:55 +0900

    io

---
+++ 14581.kt
@@ -0,0 +1,10 @@
+import java.util.*
+
+fun main(){
+    val sc = Scanner(System.`in`)
+    var a = sc.next()
+    
+    println(":fan::fan::fan:")
+    println(":fan::${a}::fan:")
+    println(":fan::fan::fan:")
+}
