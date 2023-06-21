d1b70380bc8f9efdeaa0cd98ef83e53a5887960b
ORCT <sonic407@naver.com>
2023-06-21 19:59:44 +0900

    edit(use list comprehension)

--- 1247.py
+++ 1247.py
@@ -1,4 +1,4 @@
-import sys
+'''import sys
 
 ssr = sys.stdin.readline
 
@@ -7,6 +7,20 @@ for _ in range(3):
     s = 0
     for _ in range(n):
         s += int(ssr())
+    if s == 0:
+        print('0')
+    elif s < 0:
+        print('-')
+    else:
+        print('+')'''
+        
+import sys
+
+ssr = sys.stdin.readline
+
+for _ in range(3):
+    n = int(ssr())
+    s = sum(int(ssr()) for _ in range(n))
     if s == 0:
         print('0')
     elif s < 0:
