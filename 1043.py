412fe88fabf09fcc799aea51c1bf4072c2d4fffd
ORCT <sonic407@naver.com>
2023-07-28 16:13:37 +0900

    unionfind

--- 1043.py
+++ 1043.py
@@ -29,9 +29,37 @@ knowing_truth에 값을 추가하고 다 체크하려고 했는데 그냥 사람
 파티 목록도 방문처리를 하면 이미 체크한 목록은 안돌아도 되려나?(어차피 사람을 진실 표시를 하면 자동으로 이미 체크한 파티는 안돌듯)
 
 bfs처럼 해서 queue 안에 있는 인자를 빼가지고 파티 목록을 돌도록 하면?
+
+분리 집합에 대해서 알아보자
+분리 집합(disjoint set)이란?
+분리 집합은 서로소 집합이라고도 한다. (서로소란 대수 관계에서는 1 이외의 공약수가 없는 것을 의미하지만 집합에서는 공통 원소가 없는 집합을 말한다)
+
+유니온파인드(Union-Find) : (트리)합치기&(root)찾기
+유니온 파인드 알고리즘은 그래프 탐색 알고리즘 중 하나로, 같은 연결요소인지 반대로 다른 연결요소인지를 찾을 수 있는 알고리즘이다.
+각 그룹을 트리의 형태로 표현한다
+각 노드마다 그 노드의 부모 노드 번호를 기록하여 트리 구조로 나타낸다
+이렇게 나타낸 트리는 root 노드를 갖는데, 이 root 노드를 통해 그룹을 구별할 수 있게 된다(root노드는 자기 자신을 기록하면 됨).
+
+유니온 파인드의 장점이 뭐야?
+그래프 탐색을 할 때 새로운 간선이 생길 때 마다 bfs/dfs처럼 완전 탐색(최악의 경우 완전탐색(그냥 완전 탐색은 브루트포스다)이지 정확하게는)을 진행하지 않고 같은 그룹인지 확인 가능
+지금 풀고 있는 문제에서 연결요소 체크할 때도 도움되지 않을까?
+이건 분명 사용처가 무궁무진하다.
+확실한 건 반복해서 그룹확인을 해야할 경우에 연산량을 많이 줄여줄 것
+
+Find의 시간복잡도가 트리의 높이(깊이)에 의해 결정되므로
+새로운 노드를 연결할 때 연결하는 노드의 부모 노드를 root 노드로 만들어 준다면(root에 바로 붙인다면) 보다 효율적일 것이다.
+이 최적화 방법을 경로 압축(path compression)이라고 한다.
+
+그래서 최적화 방법으로 union by rank가 있다
+높이가 낮은 트리를 높은 트리에 붙여서 시간 복잡도를 줄일 수 있다.
+높은 트리를 낮은 트리에 붙이는 경우 합친 이후의 트리의 높이가 높은 트리와 같거나 1만큼 늘어날 수 있음
+반대로 낮은 트리를 높은 트리에 붙이면 전체 트리의 높이가 변하지 않기 때문에 시간복잡도가 증가하지 않는다.
+따라서 낮은 쪽을 높은 쪽에 붙이는 것이 더 효율적이다.
+
+그러면 이제 이 문제에 유니온 파인드를 한 번 적용해볼까?
 '''
 
-from collections import deque
+'''from collections import deque
 import sys
 ssr = sys.stdin.readline
 
@@ -59,4 +87,45 @@ for party in parties:
         if visited[i] == 1:
             m -= 1
             break
-print(m)
+print(m)'''
+
+# Union-Find
+import sys
+ssr = sys.stdin.readline
+
+def union(node1, node2):
+    root1, root2 = find(node1), find(node2)
+    if root1 != root2:
+        if rank[root1] < rank[root2]:
+            uf[root1] = root2
+        elif rank[root1] == rank[root2]:
+            uf[root1] = root2
+            rank[root2] += 1
+        else:
+            uf[root2] = root1
+    
+def find(node):
+    if uf[node] == node:
+        return node
+    else:
+        uf[node] = find(uf[node])
+        return uf[node]
+
+n, m = map(int, ssr().split())
+knowing_truth = list(map(int, ssr().split()))
+parties = [list(map(int, ssr().split())) for _ in range(m)]
+uf = [i for i in range(n+1)]
+rank = [0 for _ in range(n+1)]
+if knowing_truth[0] != 0:
+    for i in knowing_truth[1:]:
+        uf[i] = knowing_truth[1]
+    rank[knowing_truth[1]] = 1
+cnt = 0
+for people in parties:
+    for i in people[1:]:
+        union(people[1], i)
+for people in parties:
+    if knowing_truth[0] != 0:
+        if uf[find(knowing_truth[1])] == uf[find(people[1])]:
+            cnt += 1
+print(m-cnt)
