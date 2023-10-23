33440d0cb4e5ea30e77c13d0786e58c41c2b5eaf
ORCT <sonic407@naver.com>
2023-10-23 16:41:08 +0900

    add floyd-warshall

--- 14938.py
+++ 14938.py
@@ -4,6 +4,9 @@
 그 중에서 최댓값
 '''
 
+'''
+# dijkstra
+
 import sys; ssr = sys.stdin.readline
 import heapq
 INF = 16
@@ -36,4 +39,35 @@ for start in range(1, n+1):
             item_cnt += items[i]
     if item_cnt > ans:
         ans = item_cnt
+print(ans)'''
+
+# floyd-warshall
+
+import sys; ssr = sys.stdin.readline
+INF = 16
+
+n, m, r = map(int, ssr().split())
+items = list(map(int, ssr().split()))
+routes = [[] for _ in range(n+1)]
+min_dist = [[INF for _ in range(n+1)] for _ in range(n+1)]
+for _ in range(r):
+    a, b, l = map(int, ssr().split())
+    routes[a].append((b, l))
+    routes[b].append((a, l))
+    min_dist[a][b] = min(min_dist[a][b], l)
+    min_dist[b][a] = min(min_dist[b][a], l)
+for i in range(n+1):
+    min_dist[i][i] = 0
+for mid in range(n+1):
+    for start in range(n+1):
+        for end in range(n+1):
+            min_dist[start][end] = min(min_dist[start][end], min_dist[start][mid]+min_dist[mid][end])
+ans = 0
+for i in range(n+1):
+    tmp = 0
+    for j in range(n+1):
+        if min_dist[i][j] <= m:
+            tmp += items[j-1]
+    if ans < tmp:
+        ans = tmp
 print(ans)
